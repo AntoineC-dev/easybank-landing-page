0f512def32280aa1f374f102dad73c38e2f38a56
Antoine Cheminat <ac.dev@hotmail.com>
2022-09-18 10:30:27 -0400

    Create Navbar component

---
+++ src/data/navigation.ts
@@ -0,0 +1,7 @@
+export const NAVIGATION_ITEMS = [
+  { label: 'Home', href: '#' },
+  { label: 'About', href: '#' },
+  { label: 'Contact', href: '#' },
+  { label: 'Blog', href: '#' },
+  { label: 'Careers', href: '#' },
+];
