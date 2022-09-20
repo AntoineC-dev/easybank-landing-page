e00c3af8c26c336a0e612dd49ecef670604c3b3c
Antoine Cheminat <ac.dev@hotmail.com>
2022-09-20 18:08:11 -0400

    Add aria-label to social icons

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -1,7 +1,7 @@
 export const SOCIALS = [
-  { icon: 'facebook', href: '#' },
-  { icon: 'youtube', href: '#' },
-  { icon: 'twitter', href: '#' },
-  { icon: 'pinterest', href: '#' },
-  { icon: 'instagram', href: '#' },
+  { icon: 'facebook', href: '#', label: 'Facebook' },
+  { icon: 'youtube', href: '#', label: 'Youtube' },
+  { icon: 'twitter', href: '#', label: 'Twitter' },
+  { icon: 'pinterest', href: '#', label: 'Pinterest' },
+  { icon: 'instagram', href: '#', label: 'Instagram' },
 ];
