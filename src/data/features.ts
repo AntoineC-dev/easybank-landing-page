7f026ad5132818779a2121464f057c20e73d285a
Antoine Cheminat <ac.dev@hotmail.com>
2022-09-20 16:49:23 -0400

    Use optimized svgs in features

--- src/data/features.ts
+++ src/data/features.ts
@@ -1,27 +1,22 @@
-import apiSvg from '$assets/icon-api.svg';
-import budgetingSvg from '$assets/icon-budgeting.svg';
-import onboardingSvg from '$assets/icon-onboarding.svg';
-import onlineSvg from '$assets/icon-online.svg';
-
 export const FEATURES = [
   {
     title: 'Online Banking',
     text: 'Our modern web and mobile applications allow you to keep track of your finances wherever you are in the world.',
-    image: onlineSvg,
+    icon: 'online',
   },
   {
     title: 'Simple Budgeting',
     text: 'See exactly where your money goes each month. Receive notifications when you’re close to your hitting limits.',
-    image: budgetingSvg,
+    icon: 'budgeting',
   },
   {
     title: 'Fast Onboarding',
     text: 'We don’t do branches. Open your account in minutes online and start taking control of your finances right away.',
-    image: onboardingSvg,
+    icon: 'onboarding',
   },
   {
     title: 'Open API',
     text: 'Manage your savings, investments, pension, and much more from one account. Tracking your money has never been easier.',
-    image: apiSvg,
+    icon: 'api',
   },
 ];
