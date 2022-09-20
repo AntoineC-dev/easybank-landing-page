a5935e53060ebbb1fbf4d85652bab3b827c5bc70
Antoine Cheminat <ac.dev@hotmail.com>
2022-09-20 16:39:02 -0400

    Create Footer component

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -5,3 +5,12 @@ export const NAVIGATION_ITEMS = [
   { label: 'Blog', href: '#' },
   { label: 'Careers', href: '#' },
 ];
+
+export const FOOTER_NAVIGATION_ITEMS = [
+  { label: 'About Us', href: '#' },
+  { label: 'Contact', href: '#' },
+  { label: 'Blog', href: '#' },
+  { label: 'Careers', href: '#' },
+  { label: 'Support', href: '#' },
+  { label: 'Privacy Policy', href: '#' },
+];
---
+++ src/data/socials.ts
@@ -0,0 +1,7 @@
+export const SOCIALS = [
+  { icon: 'facebook', href: '#' },
+  { icon: 'youtube', href: '#' },
+  { icon: 'twitter', href: '#' },
+  { icon: 'pinterest', href: '#' },
+  { icon: 'instagram', href: '#' },
+];
